3a83013dfd740fd91df5ef24aa8a6c0e368e04de
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2014-05-09 11:18:54 -0400

    added our things to __init__.py

--- atmosphy/__init__.py
+++ atmosphy/__init__.py
@@ -9,3 +9,24 @@ This is an Astropy affiliated package.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
+
+import os
+from shutil import copyfile
+#import initialize
+import logging
+
+atmosphyUserPath = os.path.expanduser('~/.atmosphy')
+moduleDir = os.path.dirname(os.path.realpath(__file__))
+
+# Generate the once-off ~/.atmosphy directory
+
+if not os.path.exists(atmosphyUserPath):
+    os.makedirs(atmosphyUserPath)
+    
+#Creating a atmosphy.db3
+if not os.path.exists(os.path.join(atmosphyUserPath, 'atmosphy.db3')):
+    import sqlite3
+    conn = sqlite3.connect(os.path.join(atmosphyUserPath, 'atmosphy.db3'))
+    conn.executescript(file(os.path.join(moduleDir, 'conf.d', 'atmosphy.schema')).read())
+    conn.commit()
+    conn.close()
